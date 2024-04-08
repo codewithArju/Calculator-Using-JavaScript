d1aa767399f1ed090bcbf7db164b63ebc2d79ced
codewithArju <arjumallick289@gmail.com>
2024-04-08 11:41:52 +0530

    calculator

---
+++ myScript.js
@@ -0,0 +1,22 @@
+let expression = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            expression = eval(expression);
+            document.querySelector('.input').value = expression;
+        } else if (e.target.innerHTML == 'C') {
+            expression = "";
+            document.querySelector('.input').value = expression;
+        }
+
+        else if (e.target.innerHTML == 'CE') {
+            expression = "0";
+            document.querySelector('.input').value = expression;
+        }
+        else {
+            expression += e.target.innerHTML;
+            document.querySelector('.input').value = expression;
+        }
+    })
+})
